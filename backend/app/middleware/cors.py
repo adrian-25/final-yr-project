df06adf96bd9726651a6c300b5b8f6093e9af2c4
adrian-25 <adrian-25@users.noreply.github.com>
2026-09-07 13:03:22 +0530

    gg

--- backend/app/middleware/cors.py
+++ backend/app/middleware/cors.py
@@ -5,6 +5,7 @@ def setup_cors(app: FastAPI):
     origins = [
         "http://localhost:5173",
         "http://localhost:3000",
+        "https://final-yr-project-zeta.vercel.app",
     ]
     app.add_middleware(
         CORSMiddleware,
